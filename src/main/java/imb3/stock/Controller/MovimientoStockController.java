04105a9f2027b9bc0aa5c0e1451f7dc878f1b295
LucasLecea <lucaslecea2003@gmail.com>
2023-06-21 01:39:05 -0300

    Agrego excepciones

--- src/main/java/imb3/stock/Controller/MovimientoStockController.java
+++ src/main/java/imb3/stock/Controller/MovimientoStockController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,8 @@ import imb.progra.arq.controllers.APIResponse;
 import imb.progra.arq.entity.Categoria;
 import imb3.stock.Service.IMovimientoService;
 import imb3.stock.entity.MovimientoStock;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 //localhost:8080/api/imb3/movimiento
 
@@ -109,6 +112,16 @@ public class MovimientoStockController {
 				return true;
 			}
 		}
+	}
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<APIResponse<?>> handleException(ConstraintViolationException ex){
+		List<String> errors = new ArrayList<>();
+		for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+			errors.add(violation.getMessage());
+		}
+		APIResponse<MovimientoStock> response = new APIResponse<MovimientoStock>(HttpStatus.BAD_REQUEST.value(), errors, null);
+		return ResponseEntity.badRequest().body(response);
 	}
 
 }
--- src/main/java/imb3/stock/entity/MovimientoStock.java
+++ src/main/java/imb3/stock/entity/MovimientoStock.java
@@ -4,14 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 @Entity
 public class MovimientoStock {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idMovimiento;
+	@Min(value = 0, message = "El ´tipo´ debe ser mayor o igual a 0")
 	private int tipo;
 	private int idCategoria;
+	@Max(value = 100, message = "La mayor cantidad de productos a vender o comprar es de 100")
 	private int cantidad;
 	private int idProducto;
 	private int idEmpleado;
