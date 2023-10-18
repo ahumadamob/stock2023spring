1a03cf7211521765f6da611873729d541cac8f40
FacundoCortez20 <facundocortezroa@gmail.com>
2023-10-18 18:00:12 -0300

    cambios en la rama empleados

--- src/main/java/imb/pr2/stock/controller/EmpleadoController.java
+++ src/main/java/imb/pr2/stock/controller/EmpleadoController.java
@@ -26,74 +26,59 @@ public class EmpleadoController {
 	IEmpleadoService service;
 
 	@GetMapping
-	public ResponseEntity<APIResponse<List<Empleado>>> mostrarTodos() {
-		APIResponse<List<Empleado>> response = new APIResponse<List<Empleado>>(200, null, service.buscarEmpleados());
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
+	
+    public ResponseEntity<APIResponse<List<Empleado>>> mostrarTodos() { 
+        List<Empleado> proveedores = service.buscar(); 
+        if(!proveedores.isEmpty()) {
+            return ResponseUtil.success(proveedores); 
+        }
+        else { 
+            return ResponseUtil.notFound("No se encontraron empleados."); 
+        }
+   }
 
 	@GetMapping("/{id}")
-	public ResponseEntity<APIResponse<Empleado>> mostrarEmpleadoPorId(@PathVariable("id") Integer id) {
-		if (service.existe(id)) {
-			Empleado empleado = service.buscarEmpleadoPorId(id);
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.OK.value(), null, empleado);
-			return ResponseEntity.status(HttpStatus.OK).body(response);
-		} else {
-			List<String> messages = new ArrayList<>();
-			messages.add("No se encontró el Empleado con id = " + id.toString());
-			messages.add("Revise nuevamente el parámetro");
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}
-	}
+	
+	  public ResponseEntity<APIResponse<Empleado>> mostrarEmpleadoPorId(@PathVariable Integer id) {
+        if(service.existe(id)) {
+            return ResponseUtil.success(service.buscarPorId(id));
+        }else {
+            return ResponseUtil.notFound("No se encontro el empleado con el id: "+id+".");}
+        }
 
 	@PostMapping
-	public ResponseEntity<APIResponse<Empleado>> crearEmpleado(@RequestBody Empleado empleado) {
-		if (service.existe(empleado.getId())) {
-			List<String> messages = new ArrayList<>();
-			messages.add("Ya existe un empleado con el ID = " + empleado.getId().toString());
-			messages.add("Para actualizar utilice el verbo PUT");
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		} else {
-			service.guardarEmpleado(empleado);
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.CREATED.value(), null, empleado);
-			return ResponseEntity.status(HttpStatus.CREATED).body(response);
-		}
-	}
 	
-	@PutMapping	
+    public ResponseEntity<APIResponse<Empleado>> crearProveedor(@RequestBody Empleado empleado) {
+        if(service.existe((empleado.getId()))) {
+            return ResponseUtil.badRequest("Ya existe el proveedor con el id: "+empleado.getId()+".");
+        }else {
+            return ResponseUtil.created(service.guardar(empleado));
+        }
+    }
+
+	@PutMapping
 	public ResponseEntity<APIResponse<Empleado>> modificarEmpleado(@RequestBody Empleado empleado) {
-		if(service.existe(empleado.getId())) {
-			service.guardarEmpleado(empleado);
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.OK.value(), null, empleado);
-			return ResponseEntity.status(HttpStatus.OK).body(response);
-		}else {
-			List<String> messages = new ArrayList<>();
-			messages.add("No existe un empleado con el ID especificado");
-			messages.add("Para crear uno nuevo utilice el verbo POST");
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}
+        if(service.existe(empleado.getId())) {
+            return ResponseUtil.success(service.guardar(empleado));
+        }
+        else {
+            return ResponseUtil.badRequest("No existe el proveedor con el id: "+empleado.getId()+".");
+        }
 
-	}
-	
-	@DeleteMapping("/{id}")	
+    }
+
+	@DeleteMapping("/{id}")
+
 	public ResponseEntity<APIResponse<Empleado>> eliminarEmpleado(@PathVariable("id") Integer id) {
-		if(service.existe(id)) {
-			service.eliminarEmpleado(id);
-			List<String> messages = new ArrayList<>();
-			messages.add("El Empleado que figura en el cuerpo ha sido eliminado") ;			
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.OK.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.OK).body(response);	
-		}else {
-			List<String> messages = new ArrayList<>();
-			messages.add("No existe un empleado con el ID = " + id.toString());
-			APIResponse<Empleado> response = new APIResponse<Empleado>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);			
-		}
-		
-	}
+        if(service.existe(id)) {
+            Empleado empleadoEliminado = service.buscarPorId(id);
+            service.eliminar(id);
+            return ResponseUtil.success(empleadoEliminado);
 
+        }else {
+            return ResponseUtil.badRequest("No existe el proveedor con el id: "+id+".");
+        }
 
+    }
 
 }
